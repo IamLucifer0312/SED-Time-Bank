d8be7fb20c4e05f4c666e5e69343c85dd7c30a3c
cuducquang <112458703+cuducquang@users.noreply.github.com>
2023-12-29 12:52:08 +0700

    update "field" -> "ìnformation"
    update variable name

--- src/Systems/MenuSystem/Interfaces/MemberMenu.cpp
+++ src/Systems/MenuSystem/Interfaces/MemberMenu.cpp
@@ -5,12 +5,12 @@
 // member menu
     void MenuSystem::member_menu()
     {
-        std::string field;
+        std::string information;
         std::string answer;
         Users::Member current_member = userSystem.get_current_member();
         while (loggedIn)
         {
-            std::cout << "\nMember actions:\n"
+            std::cout << "\n\nMember actions:\n"
                         << "==============================\n"
                         << "0. Logout.\n"
                         << "1. View my information.\n"
@@ -23,18 +23,21 @@
             switch (prompt_choice(0, 6))
             {
             case 1:
+                clear_screen();
+                // std::cout << '\n';
                 current_member.show_member_info();
-                std::cout << "Do you want to change information (Y/N): ";
+                std::cout << "\nDo you want to change information (Y/N): ";
                 std::getline(std::cin, answer);
                 if (answer == "Y"){
-                    std::cout << "Which field do you want to update: ";
-                    std::getline(std::cin, field);
-                    update_member_info(field);
+                    std::cout << "\nWhich information do you want to update: ";
+                    std::getline(std::cin, information);
+                    update_member_info(information);
                     current_member = userSystem.get_current_member();
+                    std::cout << "Update Successfully";
                 } else if (answer == "N"){
                     break;
                 } else {
-                    std::cout << "Invalid input.\n";
+                    std::cerr << "Please type Y for Yes or N for No\n";
                 }
                 break;
             case 2:
--- src/Systems/MenuSystem/Interfaces/UpdateInfo.cpp
+++ src/Systems/MenuSystem/Interfaces/UpdateInfo.cpp
@@ -1,34 +1,34 @@
 #include "../MenuSystem.hpp"
 #include "../../UserSystem/UserSystem.hpp"
 
-void MenuSystem::update_member_info(std::string field)
+void MenuSystem::update_member_info(std::string information)
 {
     std::string new_info;
-    std::cout << "Enter new " << field << ": ";
+    std::cout << "Enter new information: " << information << ": ";
     std::getline(std::cin, new_info);
-    if (field == "Full name")
+    if (information == "Full name")
     {
         userSystem.get_current_member().set_full_name(new_info);
     }
-    else if (field == "Phone number")
+    else if (information == "Phone number")
     {
         userSystem.get_current_member().set_phone_number(new_info);
     }
-    else if (field == "Home address")
+    else if (information == "Home address")
     {
         userSystem.get_current_member().set_home_address(new_info);
     }
-    else if (field == "email")
+    else if (information == "email")
     {
         userSystem.get_current_member().set_email(new_info);
     }
-    else if (field == "city")
+    else if (information == "city")
     {
         userSystem.get_current_member().set_city(new_info);
     }
     else
     {
-        std::cout << "Invalid field.\n";
+        std::cout << "Invalid input.\n";
     }
     userSystem.update_current_member();
 }
