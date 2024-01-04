889367b8a197edd62bf44eceef3f8399c44e5d9c
IamLucifer0312 <117656374+IamLucifer0312@users.noreply.github.com>
2024-01-04 17:45:55 +0700

    update interface query

--- src/Systems/MenuSystem/Interfaces/find_supporter_city.cpp
+++ src/Systems/MenuSystem/Interfaces/find_supporter_city.cpp
@@ -10,10 +10,30 @@ void MenuSystem::find_supporter_city(std::string role)
 
     switch (prompt_choice(1, 2)){
         case 1:
+            clear_screen();
             show_members_for_city("Ha noi", role);
+            std::cout << "What do you want to do ?\n"
+                            << "1. Book\n"
+                            << "0. no\n";
+            switch(prompt_choice(0,1)){
+                case 1:
+                    break;
+                case 0:
+                    break;
+            }
             break; 
         case 2:
+            clear_screen();
             show_members_for_city("Sai gon", role);
+            std::cout << "What do you want to do ?\n"
+                            << "1. Book\n"
+                            << "0. no\n";
+            switch(prompt_choice(0,1)){
+                case 1:
+                    break;
+                case 0:
+                    break;
+            }
             break;
     }
 }
