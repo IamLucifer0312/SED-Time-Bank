de6d5af55afc232fddc63b4b66039a1b1c3be59b
IamLucifer0312 <117656374+IamLucifer0312@users.noreply.github.com>
2024-01-10 16:47:19 +0700

    fix vector index

--- src/Systems/MenuSystem/Interfaces/AddAvailableJob.cpp
+++ src/Systems/MenuSystem/Interfaces/AddAvailableJob.cpp
@@ -5,12 +5,13 @@ void MenuSystem::add_available_job()
     std::string supporter_name;
     std::string skill_name;
     
-    std::cout << "Do you want to add or update your avalable job? "
-              << "1. Yes."
-              << "2. No.";
+    std::cout << "Do you want to add or update your avalable job? \n"
+              << "1. Yes. \n"
+              << "2. No. \n";
 
     std::string start_time;
     std::string end_time;
+    int arraySize = userSystem.get_current_member().get_available_times().size();
 
     switch (prompt_choice(1, 2))
     {
@@ -21,20 +22,21 @@ void MenuSystem::add_available_job()
             std::cout << "\nEnter your skill name: ";
             std::getline(std::cin, skill_name);
 
-            // for (int i = 0; i < sizeof(userSystem.get_current_member().get_available_times()); i++)
-            // {
-            //     std::cout << i + 1 << std::endl;
-            //     std::cout << userSystem.get_current_member().get_available_times()[i].get_start_time_string() << std::endl;
-            //     std::cout << userSystem.get_current_member().get_available_times()[i].get_end_time_string() << std::endl;
-            //     std::cout << std::endl;
-            // }
 
-            for (Period availableTime: userSystem.get_current_member().get_available_times())
+            for (size_t i = 0; i < arraySize; i++)
             {
-                std::cout << availableTime.get_start_time_string() << std::endl;
-                std::cout << availableTime.get_end_time_string() << std::endl;
+                std::cout << i + 1 << std::endl;
+                std::cout << userSystem.get_current_member().get_available_times()[i].get_start_time_string() << std::endl;
+                std::cout << userSystem.get_current_member().get_available_times()[i].get_end_time_string() << std::endl;
                 std::cout << std::endl;
             }
+
+            // for (Period availableTime: userSystem.get_current_member().get_available_times())
+            // {
+            //     std::cout << availableTime.get_start_time_string() << std::endl;
+            //     std::cout << availableTime.get_end_time_string() << std::endl;
+            //     std::cout << std::endl;
+            // }
 
             std::getline(std::cin, skill_name);
             break;
