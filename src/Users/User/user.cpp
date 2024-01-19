5ce9e9f556d9234a64103c7a999b9ee5933cd5b4
IamLucifer0312 <117656374+IamLucifer0312@users.noreply.github.com>
2024-01-19 23:39:08 +0700

    remove from_map and to_map functions

--- src/Users/User/user.cpp
+++ src/Users/User/user.cpp
@@ -27,18 +27,3 @@ bool Users::User::authenticate(const string &pwd) const
     return this->password == pwd;
 }
 
-// extract data from map
-void Users::User::from_map(std::map<string, string> map)
-{
-    this->username = map["username"];
-    this->password = map["password"];
-}
-
-// convert data to map
-std::map<string, string> Users::User::to_map()
-{
-    std::map<string, string> map;
-    map.emplace("username", this->username);
-    map.emplace("password", this->password);
-    return map;
-}
